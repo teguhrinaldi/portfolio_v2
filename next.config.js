63d7b3d0a72df7bbd2094c0f89ecbeb25d4b64ed
imyoursatan <teguhrinaldi23@gmail.com>
2025-01-22 23:53:26 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
-	reactStrictMode: true,
+	output: 'export', // Menghasilkan build statis
+	reactStrictMode: true, // Menjalankan React Strict Mode untuk membantu menemukan masalah potensial
 	images: {
-		unoptimized: true,
+		unoptimized: true, // Memastikan gambar tidak dioptimalkan, sesuai untuk build statis
 	},
-	assetPrefix: './',
-	basePath: '',
+	assetPrefix: './', // Menyesuaikan path aset untuk penggunaan lokal
+	basePath: '', // Jika menggunakan basePath untuk subdirektori, bisa diatur di sini
+	trailingSlash: true, // Menambahkan trailing slash di akhir URL (memungkinkan akses yang lebih konsisten di server statis)
 };
 
 module.exports = nextConfig;
