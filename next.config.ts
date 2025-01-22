fa21d3d0774631b15766fe54e0087c95e6d8d372
imyoursatan <teguhrinaldi23@gmail.com>
2025-01-22 15:58:37 +0700

    resssolveee

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
